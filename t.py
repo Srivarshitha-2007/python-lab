9397f1f06b13db9ca68265049e1c7caf9561e65b
Srivarshitha-2007 <srivarshithamaripi2007@gmail.com>
2025-12-14 21:37:23 +0530

    Add area calculation for equilateral triangle

---
+++ t.py
@@ -0,0 +1,7 @@
+#24331A05H2
+#AREA OF TRIANGLE
+
+b,h=map(float,input("enter the base and height of equilateral triangle ").split(","))
+area=0.5*b*h
+print("area of equilateral triangle is:",area)
+
