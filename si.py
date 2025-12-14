26ebde1a65cba10b16910e517df3c68491cb96d5
Srivarshitha-2007 <srivarshithamaripi2007@gmail.com>
2025-12-14 20:57:37 +0530

    Create si.py

---
+++ si.py
@@ -0,0 +1,8 @@
+#24331A05H2
+#caluculate simple intrest
+
+P=int(input("enter the principal amount"))
+T=int(input("enter the  time"))
+R=int(input("enter rate of interest "))
+si=(P*T*R)/100
+print(si)
