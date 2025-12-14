cea750abf45e5c69f53f0c25364d9a866f6f2233
Srivarshitha-2007 <srivarshithamaripi2007@gmail.com>
2025-12-14 18:30:34 +0530

    Create datatype.py to illustrate data types
    This script illustrates various data types in Python by taking user input and printing the type of each input.

---
+++ datatype.py
@@ -0,0 +1,11 @@
+#24331A05H2
+#illustrate data types 
+
+name=input("enter name")
+print(name,type(name))
+roll=int( input("enter number"))
+print(roll,type(roll))
+f=float (input("enter float number"))
+print(f,type(f))
+c=input("enter char")
+print(c,type(c))
