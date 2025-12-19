7558203d344b3238ad930bcf5a632d1af6d6f618
Srivarshitha-2007 <srivarshithamaripi2007@gmail.com>
2025-12-19 20:35:55 +0530

    Add 2D array operations in 2d.py

---
+++ 2d.py
@@ -0,0 +1,11 @@
+#24331A05H2
+#2D ARRAY OPERATIONS
+import numpy as np
+l1=np.array([[1,4,2,5],[3,6,9,7]])
+l2=np.array([[5,8,9,2],[1,5,2,4]])
+print("1st array",l1)
+print("2nd array",l2)
+print("the sum of 2D array:",l1+l2)
+print("the subtraction of 2D  array",l1-l2)
+print("the multiplication of 2D array",l1*l2)
+print("the indexing ",l1[1][1])
